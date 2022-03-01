dcc004bb5f9f0c19b117ba33a4f8ad76d5fb4079
Joyce Ruobing Zhang <81496340+JoyceRuobingZhang@users.noreply.github.com>
2022-02-28 19:07:55 -0600

    only list auth-user paymenttypes

--- bangazon_api/views/payment_type_view.py
+++ bangazon_api/views/payment_type_view.py
@@ -18,7 +18,8 @@ class PaymentTypeView(ViewSet):
     })
     def list(self, request):
         """Get a list of payment types for the current user"""
-        payment_types = PaymentType.objects.all()
+        user_id = request.auth.user.id
+        payment_types = PaymentType.objects.filter(customer__id = user_id)
         serializer = PaymentTypeSerializer(payment_types, many=True)
         return Response(serializer.data)
 
