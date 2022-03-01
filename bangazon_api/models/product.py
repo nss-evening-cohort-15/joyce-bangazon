554b0f1e962c97705b38a6e4467883a27aeca2a2
Joyce Ruobing Zhang <81496340+JoyceRuobingZhang@users.noreply.github.com>
2022-02-28 20:02:40 -0600

    avg-rating Division by zero error on products #9

--- bangazon_api/models/product.py
+++ bangazon_api/models/product.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     store = models.ForeignKey(
         "Store", on_delete=models.CASCADE, related_name='products')
     price = models.FloatField(validators=[
-        MinValueValidator(0.00), MaxValueValidator(10000.00)])
+        MinValueValidator(0.00), MaxValueValidator(17500.00)])
     description = models.TextField()
     quantity = models.IntegerField()
     location = models.CharField(max_length=100)
@@ -32,7 +32,11 @@ class Product(models.Model):
         for rating in self.ratings.all():
             total_rating += rating.score
 
-        avg = total_rating / self.ratings.count()
+        try:
+            avg = total_rating / self.ratings.count() 
+            # ‘/’ 除号 
+        except ZeroDivisionError:
+            avg = 0
         return avg
 
     @property
